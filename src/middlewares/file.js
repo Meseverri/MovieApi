471afa77b9856fbf2907869a15abd4d26dde07cd
Meseverri <meseverrimarquez5@gmail.com>
2024-07-10 16:26:08 +0200

    storage function created

--- src/middlewares/file.js
+++ src/middlewares/file.js
@@ -1,25 +1,20 @@
 const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const storage=(folderName)=>{
+  return new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+      folder: folderName,
+      allowedFormats: ["jpg", "png", "jpeg", "gif", "webp"],
+    },
+  });
+}
 
-const movieStorage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "Movies",
-    allowedFormats: ["jpg", "png", "jpeg", "gif", "webp"],
-  },
-});
-
+const movieStorage= storage("Movies")
 const MovieUpload = multer({ storage: movieStorage });
 
-const plataformStorage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: "Plataforms",
-    allowedFormats: ["jpg", "png", "jpeg", "gif", "webp"],
-  },
-});
-
+const plataformStorage=storage("Plataforms")
 const PlataformUpload = multer({ storage:plataformStorage });
 
 module.exports = { MovieUpload,PlataformUpload };
